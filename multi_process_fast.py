64fcb0f4bd0704c2d3630068c9d8a44f1673ebfd
ahmednust811 <ahmednust811@gmail.com>
2023-01-23 19:49:43 -0800

    comments

--- multi_process_fast.py
+++ multi_process_fast.py
@@ -22,7 +22,7 @@ from multiprocessing import freeze_support, Process,Manager
 import sklearn.preprocessing as normal
 
 file_mp3 = 1
-
+#function to read file and get the return value as normalized np array or original
 def read(f, normalized=True):
     """MP3 to numpy array"""
     a = AudioSegment.from_file(f)
@@ -34,7 +34,7 @@ def read(f, normalized=True):
         return a.frame_rate, normal.minmax_scale(y,feature_range = (-1,1),copy = False)#np.float32(y) / 2**15
     else:
         return a.frame_rate, y
-
+#function not implemented but send the email to the email adress
 def send_email():
     msg = EmailMessage()
     msg.set_content('This is a test message from python script')
@@ -55,14 +55,14 @@ def send_email():
     print("email sent successfuly..")
     s.quit()   
 
-
+#conver the date time now into DD/MM/YY and H:M:S
 def date_time():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Yt%H:%M:%S")
     print(dt_string)
     return dt_string.split("t")
 
-
+#insert audios into a SQL DB
 def save_audio():
     #now = datetime.now()
     #dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
@@ -80,7 +80,7 @@ def save_audio():
             conn.close() 
         except:
             pass
-
+#convert seconds to time
 def secs_to_time(secs):
     hours = secs/3600.0
     hours_f = hours - int(hours)
@@ -89,7 +89,7 @@ def secs_to_time(secs):
     seconds = minutes_f * 60
     return (int(hours),int(minutes),int(seconds))
 
-
+#load the audios of specific directory into a dictionary
 def load_audios(names,sample_directory):
     data_frame_samples ={}
     data_frame_samples[names]={'data':0,'sampling_rate':0}
@@ -98,7 +98,7 @@ def load_audios(names,sample_directory):
     data_frame_samples[names]['sampling_rate'] = sample_rate
     print("\n"+f'{names} was loaded',end="\n")
     return data_frame_samples
-
+#for time stamping to change the time 
 def time_limiters(hours,minutes,seconds):
     
     if seconds > 59:
@@ -110,7 +110,7 @@ def time_limiters(hours,minutes,seconds):
         minutes = minutes -60
         hours = hours +f_hs
     return(hours,minutes,seconds)
-
+#analyse the audios
 def analyse_audios(function_inputs,keys,l):
     data_frame_samples  = function_inputs["data_frame_samples"]
     recording_data = function_inputs["recording_data"]
@@ -123,14 +123,20 @@ def analyse_audios(function_inputs,keys,l):
     #error = np.array(error) 
 
     #for keys in data_frame_samples:
+    #load length of recording window data and sample that is loaded
     len_recording = len(recording_data)
     len_sample = len(data_frame_samples[keys]['data'])
     i = 0;
     count = 0;
     print("\n"+"analyzing: "+ str(keys))
+    #pass through the arrays in a sweeping window
     while(i+len_sample <= len_recording):
+        #calculate the correlation betweem the window and the advertisement to be analyzed
         corr = signal.correlate(data_frame_samples[keys]['data'],recording_data[i:i+len_sample])
+        #normalize the correlation for easy manipulation
         norm_corr = corr/corr.max() 
+        #threshold to decide and time stamp the advertisement, it looks at the standard deviation
+        #of correlation between to be detected and saved advertisement
         if(np.std(norm_corr)<0.03):
             offset = (np.argmax(corr) - len(data_frame_samples[keys]['data']))/sample_rate
             count = count + 1
@@ -158,20 +164,23 @@ def analyse_audios(function_inputs,keys,l):
             results[f'{keys}']['time_start'].append(f'{(s_h):02d}'+':'+f'{(s_m):02d}'+':'+f'{(s_s):02d}')
             
             
-            
+            #move the window forwrd the length of advertisement if detected
             i = i+len_sample - int(offset*sample_rate)    
         else:
-            #error=np.append(error,np.std(norm_corr))       
+            #error=np.append(error,np.std(norm_corr))
+            # move full length window forward as no ad was detected        
             i = i+len_sample
 
-               
+    #update the dictionary using list comprehension           
     l.update({keys : results[keys]})
 def main():    
    
-
+    #variable fot the directory of advertisements
     recording_direct ="C:" #"C://inetpub//wwwroot//Themediatree"    #C://add the directory you want
     while(1):
+        #create a list of items in the directory
         directory_array = [x for x in os.listdir(recording_direct+"//radiodetectionpilot")]
+        #if nothing in the directory then wait for 1minute so it can be populated
         if len(directory_array) > 0:
             for recording_directory in directory_array:
                 print("Analyzing commercials for Radio Statio ID: "+recording_directory)
@@ -180,11 +189,13 @@ def main():
                 #print("")
                 
                 to_check = recording_direct+"//"+"radiodetectionpilot"+"//"+recording_directory+"//"+"recordings"+"//"+"wav_out"
+                #check the directory for mp3 extension files
                 wav_check = [f for f in os.listdir(to_check) if f.endswith('.mp3')]
                 if not wav_check:
                     print("\n nothing in "+ recording_directory + " folder  \n")
                     TIME.sleep(10)
                     continue
+                #check for sample commercials
                 sample_directory = recording_direct+"//"+"radiodetectionpilot"+"//"+recording_directory+"//"+"sample commercials"       
                 list_sample_names_mp3= [os.path.splitext(x)[0] for x in os.listdir(sample_directory) if x.endswith(".mp3")]
                 list_sample_names =[os.path.splitext(x)[0] for x in os.listdir(sample_directory+"//wav_out") if x.endswith(".mp3")]
@@ -198,9 +209,10 @@ def main():
                 
              
                 
-                
+                #list of all the files in sample directory   
                 lr = [sample_directory for _ in list_sample_names]
                 # print(lr)
+                #multiprocess operation on load audios function
                 with concurrent.futures.ProcessPoolExecutor() as executor:
                     data = executor.map(load_audios,list_sample_names,lr)
                     data_frame_samples={}
@@ -222,14 +234,14 @@ def main():
                     start_time = [int(start_t[:2]),int(start_t[2:4]),int(start_t[4:6])]
                     #date,time = date_time()
                     #start_time= [int(x) for x in time.split(':')]
-                    
+                    #multi process operation for reading the mp3 files
                     with concurrent.futures.ProcessPoolExecutor() as executor:
                         data = executor.map(read,[to_check+"//"+rec+".mp3"])
                         
                     for test in data:
                         print(len(test))
                         sample_rate,recording_data = test
-                   
+                    #create an iterable for function inputs to multiprocess - needs optimization
                     function_inputs = {"data_frame_samples": data_frame_samples,
                                         "recording_data": recording_data,
                                         "sample_rate":sample_rate,
@@ -241,7 +253,7 @@ def main():
                     list_procs = list()
                     l = Manager().dict() 
                     for keys in data_frame_samples:
-                           
+                        #create multi process operation manually   
                         p = Process(target = analyse_audios,args = (function_inputs,keys,l))
                         p.daemon =True
                         p.start()
@@ -249,10 +261,11 @@ def main():
                     for idx, w_procs in enumerate(list_procs):
                         w_procs.join()
                     #print(l)
+                    #all the values from multi process are returned to the Manager
                     results = dict(l)
                     #print(type(results))
                     
-                    
+                    #convert dictionary into data frame
                     df = pd.DataFrame(data=results)
                     ##df.index.name ='ADDID'
                     df = df.T
@@ -299,6 +312,7 @@ def main():
             
                 
 if __name__==('__main__'):
+    #to freeze the console when exe is made
     freeze_support()
     main()
     
